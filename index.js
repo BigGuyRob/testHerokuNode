1caf351bf7a4ae9e8cc321aa50d986a563510976
BrainiacR <48625392+BrainiacR@users.noreply.github.com>
2020-07-02 11:05:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
-let ejs = require('ejs')
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var express = require('express');
+var app = express();
+let ejs = require('ejs');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   ejs.renderFile('./views/index.ejs')
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
