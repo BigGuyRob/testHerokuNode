001ea6b213d856b4f5313a5a6f1706e6c351c55e
BrainiacR <48625392+BrainiacR@users.noreply.github.com>
2020-07-02 11:03:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  ejs.renderFile('index')
+  ejs.renderFile('./views/index.ejs')
 })
 
 app.listen(app.get('port'), function() {
